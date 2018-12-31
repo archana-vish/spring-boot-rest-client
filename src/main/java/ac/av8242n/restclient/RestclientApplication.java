ed804d87a27fb9534997df7b0945dedd83cc5ed3
Archana Visveswaran <A.Visveswaran@greenwich.ac.uk>
2018-12-31 06:50:57 +0000

    Initial commit

---
+++ src/main/java/ac/av8242n/restclient/RestclientApplication.java
@@ -0,0 +1,14 @@
+package ac.av8242n.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestclientApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/ac/av8242n/restclient/RestclientApplicationTests.java
@@ -0,0 +1,18 @@
+package ac.av8242n.restclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestclientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("Application context loaded..");
+	}
+
+}
+
